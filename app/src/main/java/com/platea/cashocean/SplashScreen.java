1f6b484ad08838c11a5a8369974f169c27bb9a63
Muchammad Fikri Izzuddin <=>
2016-10-29 04:59:09 +0700

    Add Splash Screen, Add Navigation Drawer, Add Some Drawable Part 2

---
+++ app/src/main/java/com/platea/cashocean/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.platea.cashocean;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Fikri on 26/10/2016.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
